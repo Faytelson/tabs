203b17fa8795c5e59b014c2df73d379f53fb8c0e
Darya <you@example.com>
2022-07-06 17:57:50 +0300

    final script

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 "use strict";
 
-const tabs = document.querySelectorAll('.tab__item');
-const pages = document.querySelectorAll('.tab__page');
-const panel = document.querySelector('.tab__panel');
+const panel = document.querySelector('.tab__panel'),
+    tabs = document.querySelectorAll('.tab__item'),
+    pages = document.querySelectorAll('.tab__page');
 
-for(let i = 0; i < pages.length; i ++) {
+for(let i = 0; i < pages.length; i++) {
     tabs[i].dataset.open = i;
 }
-panel.addEventListener('click', function(event) {
-    if(event.target.classList.contains('tab__item')) {
-        for(let i = 0; i < pages.length; i++) {
-            pages[i].classList.remove('active');
-            tabs[i].classList.remove('active');
+panel.addEventListener('click', function(event){
+    let target = event.target;
+    if(target.classList.contains('tab__item')) {
+        for(let i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove('active', 'fade');
+            pages[i].classList.remove('active', 'fade');
         }
-        pages[event.target.dataset.open].classList.add('active');
-        event.target.classList.add('active');
+        target.classList.add('active');
+        pages[target.dataset.open].classList.add('active', 'show');
     }
-});
+})
